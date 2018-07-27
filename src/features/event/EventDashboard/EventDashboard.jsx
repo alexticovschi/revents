4e3e655f10c0d31ac542f12eb81de3fb1d7889e4
AlexTicovschi <alexmticovschi@gmail.com>
2018-07-27 13:09:40 +0100

    connecting the event actions to the event dashboard

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,97 +1,133 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Grid, Button } from 'semantic-ui-react';
-import EventList from '../EventList/EventList';
-import EventForm from '../EventForm/EventForm';
-import cuid from 'cuid';
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Grid, Button } from "semantic-ui-react";
+import EventList from "../EventList/EventList";
+import EventForm from "../EventForm/EventForm";
+import cuid from "cuid";
+import { createEvent, updateEvent, deleteEvent } from "../eventActions";
 
 class EventDashboard extends Component {
   state = {
     isOpen: false,
     selectedEvent: null
-  }
+  };
 
   handleFormOpen = () => {
-    this.setState({ 
+    this.setState({
       selectedEvent: null,
-      isOpen: true 
+      isOpen: true
     });
-  }
+  };
 
   handleCancel = () => {
     this.setState({ isOpen: false });
-  }
+  };
 
-  handleUpdateEvent = (updatedEvent) => {
-    this.setState({
-      events: this.state.events.map(event => {
-        // if the selected event id is equal to the updated event id ...
-        // create a copy of the updated event and assigning it to the state 
-        // (not mutating the existing event in state),
-        // else return the event
-        return event.id === updatedEvent.id ? Object.assign({}, updatedEvent) : event
-      }),
-      isOpen: false,
-      selectedEvent: null
-    })
-  }
-
-  handleOpenEvent = (eventToOpen) => () => {
+  handleOpenEvent = eventToOpen => () => {
     this.setState({
       selectedEvent: eventToOpen,
       isOpen: true
     });
-  }
+  };
 
-  handleCreateEvent = (newEvent) => {
+  // handleCreateEvent = (newEvent) => {
+  //   newEvent.id = cuid();
+  //   newEvent.hostPhotoURL = 'assets/user.png';
+  //   const updatedEvents = [...this.state.events, newEvent];
+  //   this.setState({
+  //     events: updatedEvents,
+  //     isOpen: false
+  //   });
+  // }
+
+  // handleUpdateEvent = (updatedEvent) => {
+  //   this.setState({
+  //     events: this.state.events.map(event => {
+  //       // if the selected event id is equal to the updated event id ...
+  //       // create a copy of the updated event and assigning it to the state
+  //       // (not mutating the existing event in state),
+  //       // else return the event
+  //       return event.id === updatedEvent.id ? Object.assign({}, updatedEvent) : event
+  //     }),
+  //     isOpen: false,
+  //     selectedEvent: null
+  //   })
+  // }
+
+  // handleDeleteEvent = (eventId) => () => {
+  //   // returns all the events that do not match the event id that is passed in(the selected event)
+  //   const updatedEvents = this.state.events.filter(event => event.id !== eventId);
+  //   this.setState({
+  //     events: updatedEvents
+  //   })
+  // }
+
+  handleCreateEvent = newEvent => {
     newEvent.id = cuid();
     newEvent.hostPhotoURL = 'assets/user.png';
-    const updatedEvents = [...this.state.events, newEvent];
-    this.setState({ 
-      events: updatedEvents,
+    this.props.createEvent(newEvent);
+    this.setState({
       isOpen: false
     });
-  }
+  };
 
-  handleDeleteEvent = (eventId) => () => {
-    // returns all the events that do not match the event id that is passed in(the selected event)
-    const updatedEvents = this.state.events.filter(event => event.id !== eventId);
+  handleUpdateEvent = updatedEvent => {
+    this.props.updateEvent(updatedEvent);
     this.setState({
-      events: updatedEvents
-    })
-  }
+      isOpen: false,
+      selectedEvent: null
+    });
+  };
+
+  handleDeleteEvent = eventId => () => {
+    this.props.deleteEvent(eventId);
+  };
 
   render() {
-    const {selectedEvent} = this.state;
-    const{ events } = this.props;
+    const { selectedEvent } = this.state;
+    const { events } = this.props;
     return (
       <Grid>
         <Grid.Column width={10}>
-            <EventList 
-              deleteEvent={this.handleDeleteEvent} 
-              onEventOpen={this.handleOpenEvent} 
-              events={events}/>
+          <EventList
+            deleteEvent={this.handleDeleteEvent}
+            onEventOpen={this.handleOpenEvent}
+            events={events}
+          />
         </Grid.Column>
         <Grid.Column width={6}>
-            <Button onClick={this.handleFormOpen} positive content='Create Event'/>
-            {this.state.isOpen && (
-              <EventForm 
-                updateEvent={this.handleUpdateEvent}
-                selectedEvent={selectedEvent}
-                createEvent={this.handleCreateEvent}
-                handleCancel={this.handleCancel}/>
-            )}
+          <Button
+            onClick={this.handleFormOpen}
+            positive
+            content="Create Event"
+          />
+          {this.state.isOpen && (
+            <EventForm
+              updateEvent={this.handleUpdateEvent}
+              selectedEvent={selectedEvent}
+              createEvent={this.handleCreateEvent}
+              handleCancel={this.handleCancel}
+            />
+          )}
         </Grid.Column>
       </Grid>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     events: state.events
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps)(EventDashboard);
+const actions = {
+  createEvent,
+  updateEvent,
+  deleteEvent
+};
+
+export default connect(
+  mapStateToProps,
+  actions
+)(EventDashboard);
