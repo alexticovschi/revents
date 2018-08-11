061f4637966622e0b7c02694d5c9e2c1ef825cd0
AlexTicovschi <alexmticovschi@gmail.com>
2018-08-11 21:19:57 +0100

    setting up the basic user page form

--- src/features/user/Settings/SettingsDashboard.jsx
+++ src/features/user/Settings/SettingsDashboard.jsx
@@ -9,13 +9,13 @@ import PhotosPage from "./PhotosPage";
 import AccountPage from "./AccountPage";
 import { updatePassword } from "../../auth/authActions";
 
-const SettingsDashboard = ({ updatePassword, providerId }) => {
+const SettingsDashboard = ({ updatePassword, providerId, user }) => {
   return (
     <Grid>
       <Grid.Column width={12}>
         <Switch>
           <Redirect exact from="/settings" to="/settings/basic" />
-          <Route path="/settings/basic" component={BasicPage} />
+          <Route path="/settings/basic" render={() => <BasicPage initialValues={user} />} />
           <Route path="/settings/about" component={AboutPage} />
           <Route path="/settings/photos" component={PhotosPage} />
           <Route
@@ -37,7 +37,8 @@ const actions = {
 
 const mapStateToProps = (state) => {
   return {
-    providerId: state.firebase.auth.providerData[0].providerId
+    providerId: state.firebase.auth.providerData[0].providerId,
+    user: state.firebase.profile
   }
 }
 
