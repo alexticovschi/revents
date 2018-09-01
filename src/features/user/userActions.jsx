8e4fb06bc8000f1f6b2c212afdc7795dfcd20c64
AlexTicovschi <alexmticovschi@gmail.com>
2018-09-01 09:30:33 +0100

    dealing with duplicate photos on the page

--- src/features/user/userActions.jsx
+++ src/features/user/userActions.jsx
@@ -1,4 +1,5 @@
 import moment from 'moment';
+import cuid from 'cuid'
 import { toastr } from 'react-redux-toastr';
 
 export const updateProfile = (user) => 
@@ -19,12 +20,13 @@ export const updateProfile = (user) =>
 
 export const uploadProfileImage = (file, fileName) => 
     async (dispatch, getState, {getFirebase, getFirestore}) => {
+        const imageName = cuid();
         const firebase = getFirebase();
         const firestore = getFirestore();
         const user = firebase.auth().currentUser;
         const path = `${user.uid}/user_images`;
         const options = {
-            name: fileName
+            name: imageName
         };
         try {
             // upload the file to firebase storage
@@ -52,7 +54,7 @@ export const uploadProfileImage = (file, fileName) =>
                 doc: user.uid,
                 subcollections: [{collection: 'photos'}]
             }, {
-                name: fileName,
+                name: imageName,
                 url: downloadURL
             })
         } catch (error) {
