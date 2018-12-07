cac6ff7e57cc67d783325fe63ebefbc3acd2d8a7
AlexTicovschi <alexmticovschi@gmail.com>
2018-12-07 13:32:05 +0000

    creating the event method

---
+++ src/app/common/util/helpers.js
@@ -0,0 +1,19 @@
+export const createNewEvent = (user, photoURL, event) => {
+    event.date = moment(event.date).toDate();
+    return {
+        ...event,
+        hostUid: user.uid,
+        hostedBt: user.displayName,
+        hostPhotoURL: photoURL || '/assets/user.png',
+        created: Date.now(),
+        attendees: {
+            [user.uid]: {
+                going: true,
+                joinDate: Date.now(),
+                photoURL: photoURL || '/assets/user.png',
+                displayName: user.displayName,
+                host: true
+            }
+        }
+    }
+}
